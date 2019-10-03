84995c70c625db1dd3b6cd7eca96c730c573533b
RaySCS <rayaan.siddiqi@gmail.com>
2019-10-03 17:53:04 -0500

    Got the database working at home thursday

--- app/src/main/java/com/example/actualdatabaseproject/Activity3.java
+++ app/src/main/java/com/example/actualdatabaseproject/Activity3.java
@@ -2,6 +2,7 @@ package com.example.actualdatabaseproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -23,6 +24,10 @@ public class Activity3 extends AppCompatActivity {
     int displaySteals;
     int displayRebounds;
 
+    private static final String TAG = "Activity3";
+
+    DatabaseHelper myDB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,23 +38,38 @@ public class Activity3 extends AppCompatActivity {
         infoEntry3 = (EditText)findViewById(R.id.infoEntry3);
         infoEntry4 = (EditText)findViewById(R.id.infoEntry4);
         infoEntry5 = (EditText)findViewById(R.id.infoEntry5);
-
+        myDB = new DatabaseHelper(this);
 
     }
-
-    boolean absorbInfo = true;
 
     public void submitInfo(View view){
-        if(absorbInfo){
-            displayMinutes = Integer.parseInt(infoEntry1.getText().toString());
-            displayPoints = Integer.parseInt(infoEntry2.getText().toString());
-            displayAssists = Integer.parseInt(infoEntry3.getText().toString());
-            displaySteals = Integer.parseInt(infoEntry4.getText().toString());
-            displayRebounds = Integer.parseInt(infoEntry5.getText().toString());
-
-        }
+        displayMinutes = Integer.parseInt(infoEntry1.getText().toString());
+        AddData(Integer.toString(displayMinutes));
+//        displayPoints = Integer.parseInt(infoEntry2.getText().toString());
+//        displayAssists = Integer.parseInt(infoEntry3.getText().toString());
+//        displaySteals = Integer.parseInt(infoEntry4.getText().toString());
+//        displayRebounds = Integer.parseInt(infoEntry5.getText().toString());
 
     }
+
+
+    public void openData(View view){
+        Intent intent = new Intent(Activity3.this, ViewListContent.class);
+        startActivity(intent);
+    }
+    public void AddData(String newEntry) {
+
+        boolean insertData = myDB.addData(newEntry);
+
+        if(insertData==true){
+            Toast.makeText(this, "Data Successfully Inserted!", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(this, "Something went wrong :(.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 
 
 }
---
+++ app/src/main/java/com/example/actualdatabaseproject/ViewListContent.java
@@ -0,0 +1,42 @@
+package com.example.actualdatabaseproject;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class ViewListContent extends AppCompatActivity {
+
+    DatabaseHelper myDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_list_content);
+
+        ListView listView = (ListView) findViewById(R.id.listview);
+        myDB = new DatabaseHelper(this);
+
+        //populate an ArrayList<String> from the database and then view it
+        ArrayList<String> theList = new ArrayList<>();
+        Cursor data = myDB.getListContents();
+        if(data.getCount() == 0){
+            Toast.makeText(this, "There are no contents in this list!",Toast.LENGTH_LONG).show();
+        }
+        else{
+            while(data.moveToNext()){
+                theList.add(data.getString(1));
+                ListAdapter listAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,theList);
+                listView.setAdapter(listAdapter);
+            }
+        }
+
+
+    }
+}
