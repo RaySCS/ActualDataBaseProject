95025863c991618c7ea04e8235ad08bcf27ecb72
RaySCS <rayaan.siddiqi@gmail.com>
2019-10-03 17:11:35 -0500

    Athomelatst

--- app/src/main/java/com/example/actualdatabaseproject/Activity3.java
+++ app/src/main/java/com/example/actualdatabaseproject/Activity3.java
@@ -3,39 +3,52 @@ package com.example.actualdatabaseproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Activity3 extends AppCompatActivity {
 
-    TextView infoEntry1 = (TextView)findViewById(R.id.infoEntry1);
-    TextView infoEntry2 = (TextView)findViewById(R.id.infoEntry2);
-    TextView infoEntry3 = (TextView)findViewById(R.id.infoEntry3);
-    TextView infoEntry4 = (TextView)findViewById(R.id.infoEntry4);
-    TextView infoEntry5 = (TextView)findViewById(R.id.infoEntry5);
+    EditText infoEntry1;
+    EditText infoEntry2;
+    EditText infoEntry3;
+    EditText infoEntry4;
+    EditText infoEntry5;
 
 
-    int displayMinutes = 0;
-    int displayPoints = 0;
-    int displayAssists = 0;
-    int displaySteals = 0;
-    int displayRebounds = 0;
+    int displayMinutes;
+    int displayPoints;
+    int displayAssists;
+    int displaySteals;
+    int displayRebounds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
+
+        infoEntry1 = (EditText)findViewById(R.id.infoEntry1);
+        infoEntry2 = (EditText)findViewById(R.id.infoEntry2);
+        infoEntry3 = (EditText)findViewById(R.id.infoEntry3);
+        infoEntry4 = (EditText)findViewById(R.id.infoEntry4);
+        infoEntry5 = (EditText)findViewById(R.id.infoEntry5);
+
+
     }
 
-    public void submitInfo(){
-        displayMinutes = Integer.parseInt(infoEntry1.getText().toString());
-        displayPoints = Integer.parseInt(infoEntry2.getText().toString());
-        displayAssists = Integer.parseInt(infoEntry3.getText().toString());
-        displaySteals = Integer.parseInt(infoEntry4.getText().toString());
-        displayRebounds = Integer.parseInt(infoEntry5.getText().toString());
+    boolean absorbInfo = true;
 
+    public void submitInfo(View view){
+        if(absorbInfo){
+            displayMinutes = Integer.parseInt(infoEntry1.getText().toString());
+            displayPoints = Integer.parseInt(infoEntry2.getText().toString());
+            displayAssists = Integer.parseInt(infoEntry3.getText().toString());
+            displaySteals = Integer.parseInt(infoEntry4.getText().toString());
+            displayRebounds = Integer.parseInt(infoEntry5.getText().toString());
 
-        Toast.makeText(this, "eneterdddd", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 
