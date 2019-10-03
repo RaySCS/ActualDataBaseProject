715ef41dff2d7e7e193275f3d8f94caf3fc0716e
RaySCS <rayaan.siddiqi@gmail.com>
2019-10-03 14:22:27 -0500

    Atschool right now

--- app/src/main/java/com/example/actualdatabaseproject/Activity2.java
+++ app/src/main/java/com/example/actualdatabaseproject/Activity2.java
@@ -2,7 +2,9 @@ package com.example.actualdatabaseproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -12,9 +14,11 @@ public class Activity2 extends AppCompatActivity {
         setContentView(R.layout.activity_2);
     }
 
-    @Override
-    public void finish() {
-        super.finish();
-        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);//in order to make transition between activities look nice
+    public void openAddPlayerActivity(View view){
+        Intent intent = new Intent(this, Activity3.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);//slide to right
     }
+
+
 }
--- app/src/main/java/com/example/actualdatabaseproject/BasketballPlayer.java
+++ app/src/main/java/com/example/actualdatabaseproject/BasketballPlayer.java
@@ -2,7 +2,38 @@ package com.example.actualdatabaseproject;
 
 public class BasketballPlayer {
 
-    public BasketballPlayer(){
+    int minutes, points, assists, steals, rebounds = 0;
+    String pName = "";
+    public BasketballPlayer(String n){
+        pName = n;
+    }
 
+    public void addStats(int m, int p, int a, int s, int r){
+        minutes = m;
+        points = p;
+        assists = a;
+        steals = s;
+        rebounds = r;
+    }
+
+    public int tellPoints(){
+        return points;
+    }
+
+    public int tellMinutes(){
+        return minutes;
+    }
+
+
+    public int tellAssists(){
+        return assists;
+    }
+
+    public int tellSteals(){
+        return steals;
+    }
+
+    public int tellRebounds(){
+        return rebounds;
     }
 }
